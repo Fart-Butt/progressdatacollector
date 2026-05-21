57c393fba5e6e27f0d59a1f554da2a3cf3315dd6
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2026-05-21 17:35:57 -0400

    legacy table that is no longer needed

--- database/mysql/progress_playertracker_v2_old.sql
+++
@@ -1,9 +0,0 @@
-create table progress_playertracker_v2_old
-(
-    id        bigint auto_increment
-        primary key,
-    datetime  datetime default CURRENT_TIMESTAMP not null,
-    player    varchar(30) charset latin1         not null,
-    timedelta int                                not null
-);
-
